3af08801d3d14c183905c27b7e926184e048f18b
Brian Liao <yaramiu@gmail.com>
2023-06-13 19:36:05 -0400

    Display course info for any number of parts

---
+++ part2/course-information/src/components/Course.js
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Content from "./Content";
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header title={course.name} />
+      <Content parts={course.parts} />
+    </>
+  );
+};
+
+export default Course;
