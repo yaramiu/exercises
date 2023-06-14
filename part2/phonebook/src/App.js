72f275cae985363acfd61d6a4d15cbfbcb836531
Brian Liao <yaramiu@gmail.com>
2023-06-14 18:46:10 -0400

    Implement a name search filter

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,10 +3,14 @@ import Contact from "./component/Contact";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
 
   const handleNameChange = (event) => setNewName(event.target.value);
 
@@ -18,9 +22,8 @@ const App = () => {
     const checkNameExists = () => {
       let isExistingName = false;
       persons.forEach((person) => {
-        if (person.name.toLowerCase() === newName.toLowerCase()) {
+        if (person.name.toLowerCase() === newName.toLowerCase())
           isExistingName = true;
-        }
       });
       return isExistingName;
     };
@@ -46,9 +49,31 @@ const App = () => {
     setNewNumber("");
   };
 
+  const handleSearchChange = (event) => setSearch(event.target.value);
+
+  const filteredPersons =
+    search.length === 0
+      ? persons
+      : persons.filter((person) => {
+          const ANY_UPPER_CASE_REGEX = /^[a-z]*[A-Z]/;
+          if (search.match(ANY_UPPER_CASE_REGEX)) {
+            const caseSensitiveCheck = person.name.includes(search);
+            return caseSensitiveCheck;
+          }
+          const caseInsensitiveCheck = person.name
+            .toLowerCase()
+            .includes(search.toLowerCase());
+          return caseInsensitiveCheck;
+        });
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input onChange={handleSearchChange} value={search} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input onChange={handleNameChange} value={newName} />
@@ -61,7 +86,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {filteredPersons.map((person) => (
         <Contact key={person.name} name={person.name} number={person.number} />
       ))}
     </div>
