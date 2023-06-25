8f32dfd78db67f8409a9f5f38c5f9796f7f1ae52
Brian Liao <yaramiu@gmail.com>
2023-06-24 20:33:04 -0400

    Add totalLikes function and its tests

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,6 +1,12 @@
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
-  console.log(blogs);
   return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+  let total = 0;
+  blogs.forEach((blog) => (total += blog.likes));
+  return total;
+};
+
+module.exports = { dummy, totalLikes };
