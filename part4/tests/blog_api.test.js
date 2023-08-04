2f30774e4576e57e70ee2730b1b1d635ad095514
Brian Liao <yaramiu@gmail.com>
2023-08-03 20:45:39 -0400

    Test adding a new blog post to database

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -5,6 +5,10 @@ const Blog = require("../models/blog");
 
 const api = supertest(app);
 
+beforeEach(async () => {
+  await Blog.deleteMany({});
+});
+
 test("blogs are returned as json", async () => {
   await api
     .get("/api/blogs")
@@ -12,8 +16,30 @@ test("blogs are returned as json", async () => {
     .expect(200);
 });
 
-test("blog posts contains id property", async () => {
+test("blog posts contains id property", () => {
   expect(new Blog().id).toBeDefined();
+});
+
+test("a blog post can be added", async () => {
+  const newBlogPost = {
+    title: "npm audit: Broken by Design",
+    author: "Dan Abramov",
+    url: "https://overreacted.io/npm-audit-broken-by-design/",
+    likes: 10,
+  };
+
+  let request = await api.get("/api/blogs");
+  const blogPostsBefore = request.body;
+
+  await api.post("/api/blogs").send(newBlogPost).expect(201);
+
+  request = await api.get("/api/blogs");
+  const blogPostsAfter = request.body;
+
+  expect(blogPostsAfter).toHaveLength(blogPostsBefore.length + 1);
+
+  const titles = blogPostsAfter.map((blogPost) => blogPost.title);
+  expect(titles).toContain("npm audit: Broken by Design");
 });
 
 afterAll(async () => {
