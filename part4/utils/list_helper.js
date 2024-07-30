411046c3f88cd9e1bf52e13397f7c4b1c1726c8e
yaramiu <yaramiu@gmail.com>
2024-07-29 21:22:41 -0400

    Complete Helper Functions and Unit Tests step 3

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1;
 };
@@ -6,4 +7,22 @@ const totalLikes = (blogs) => {
   return blogs.reduce((likes, blog) => likes + blog.likes, 0);
 };
 
-export default { dummy, totalLikes };
+const favoriteBlog = (blogs) => {
+  let blogWithMaxLikes = null;
+  let currentMaxLikes = 0;
+  blogs.forEach((blog) => {
+    if (blog.likes >= currentMaxLikes) {
+      currentMaxLikes = blog.likes;
+      blogWithMaxLikes = blog;
+    }
+  });
+  return !blogWithMaxLikes
+    ? null
+    : {
+        title: blogWithMaxLikes.title,
+        author: blogWithMaxLikes.author,
+        likes: blogWithMaxLikes.likes,
+      };
+};
+
+export default { dummy, totalLikes, favoriteBlog };
