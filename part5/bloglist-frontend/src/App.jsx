0ada8b6580895c214924d329841f39e367c15f0f
yaramiu <yaramiu@gmail.com>
2024-08-03 15:57:29 -0400

    Complete Blog List Frontend step 10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -139,9 +139,11 @@ const App = () => {
         <h2>create new</h2>
         <BlogForm createBlog={addBlog} />
       </Togglable>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} addLikes={addLikes} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} addLikes={addLikes} />
+        ))}
     </div>
   );
 };
