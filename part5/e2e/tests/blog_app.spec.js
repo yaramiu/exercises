bd6e4228e463e05f4d3dd77fc9858ad32d2b5102
yaramiu <yaramiu@gmail.com>
2024-08-06 16:11:49 -0400

    Complete Blog List End To End Testing step 3

--- part5/e2e/tests/blog_app.spec.js
+++ part5/e2e/tests/blog_app.spec.js
@@ -36,5 +36,32 @@ describe("Blog app", () => {
       await expect(errorDiv).toHaveCSS("border-style", "solid");
       await expect(errorDiv).toHaveCSS("color", "rgb(255, 0, 0)");
     });
+
+    describe("When logged in", () => {
+      beforeEach(async ({ page }) => {
+        await loginWith(page, "mluukkai", "salainen");
+      });
+
+      test("a new blog can be created", async ({ page }) => {
+        await page.getByRole("button", { name: "create new blog" }).click();
+
+        await page
+          .getByPlaceholder("blog title")
+          .fill("The Cloud Is Just Someone Else's Computer");
+        await page.getByPlaceholder("blog author").fill("Jeff Atwood");
+        await page
+          .getByPlaceholder("blog url")
+          .fill(
+            "https://blog.codinghorror.com/the-cloud-is-just-someone-elses-computer/"
+          );
+        await page.getByRole("button", { name: "create" }).click();
+
+        await expect(
+          page.getByText(
+            "The Cloud Is Just Someone Else's Computer Jeff Atwood"
+          )
+        ).toBeVisible();
+      });
+    });
   });
 });
