49388cec34046aa125e751e68bf06fb584bfa405
yaramiu <yaramiu@gmail.com>
2024-08-16 14:14:39 -0400

    Complete Patientor backend step 2

--- part8/patientor-backend/build/index.js
+++ part8/patientor-backend/build/index.js
@@ -4,8 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const PORT = 3000;
+const cors_1 = __importDefault(require("cors"));
+const PORT = 3001;
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.get("/api/ping", (_request, response) => {
     console.log("someone pinged here");
--- part8/patientor-backend/index.ts
+++ part8/patientor-backend/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 
-const PORT = 3000;
+const PORT = 3001;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/api/ping", (_request, response) => {
