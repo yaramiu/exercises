83bd26d61bce86a3eddae6d7eed05d2c944abf1b
yaramiu <yaramiu@gmail.com>
2024-08-18 19:13:38 -0400

    Complete 9.14

---
+++ part8/course-information/src/App.tsx
@@ -0,0 +1,57 @@
+interface CoursePart {
+  name: string;
+  exerciseCount: number;
+}
+
+const Header = ({ name }: { name: string }) => {
+  return <h1>{name}</h1>;
+};
+
+const Content = ({ courseParts }: { courseParts: CoursePart[] }) => {
+  return (
+    <>
+      {courseParts.map((coursePart) => (
+        <p>
+          {coursePart.name} {coursePart.exerciseCount}
+        </p>
+      ))}
+    </>
+  );
+};
+
+const Total = ({ totalExercises }: { totalExercises: number }) => {
+  return <p>Number of exercises {totalExercises}</p>;
+};
+
+const App = () => {
+  const courseName = "Half Stack application development";
+  const courseParts = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7,
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14,
+    },
+  ];
+
+  const totalExercises = courseParts.reduce(
+    (sum, part) => sum + part.exerciseCount,
+    0
+  );
+
+  return (
+    <div>
+      <Header name={courseName} />
+      <Content courseParts={courseParts} />
+      <Total totalExercises={totalExercises} />
+    </div>
+  );
+};
+
+export default App;
