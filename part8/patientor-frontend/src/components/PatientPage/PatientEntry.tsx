0c0f6f50b88b107959cfd7c3c3c644dd5211d88c
yaramiu <yaramiu@gmail.com>
2024-08-20 17:56:31 -0400

    Complete Patientor step 5

--- part8/patientor-frontend/src/components/PatientPage/PatientEntry.tsx
+++ part8/patientor-frontend/src/components/PatientPage/PatientEntry.tsx
@@ -1,29 +1,47 @@
 import { Typography } from "@mui/material";
 import { v4 as uuid } from "uuid";
+import { useState, useEffect } from "react";
 
-import { Entry } from "../../types";
+import { Diagnosis, Entry } from "../../types";
+import diagnosisService from "../../services/diagnoses";
 
 interface Props {
   entry: Entry;
 }
 
 const PatientEntry = ({ entry }: Props) => {
+  const [diagnoses, setDiagnoses] = useState<Diagnosis[]>([]);
+
+  useEffect(() => {
+    const getDiagnoses = async () => {
+      const fetchedDiagnoses = await diagnosisService.getAll();
+      setDiagnoses(fetchedDiagnoses);
+    };
+
+    getDiagnoses();
+  }, []);
+
   return (
     <div>
       <Typography variant="body2">
         {entry.date} <i>{entry.description}</i>
       </Typography>
-      {entry.diagnosisCodes ? (
+      {
         <ul>
-          {entry.diagnosisCodes.map((diagnosisCode) => (
+          {entry.diagnosisCodes?.map((diagnosisCode) => (
             <li>
               <Typography key={uuid()} variant="body2" style={{ margin: 0 }}>
-                {diagnosisCode}
+                {diagnosisCode}{" "}
+                {
+                  diagnoses.find(
+                    (diagnosis) => diagnosis.code === diagnosisCode
+                  )?.name
+                }
               </Typography>
             </li>
           ))}
         </ul>
-      ) : null}
+      }
     </div>
   );
 };
